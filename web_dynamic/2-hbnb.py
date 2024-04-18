1d264a273259b7836994c675657ec9337bdab419
pasebukky <olubukunolapase@gmail.com>
2024-04-18 08:45:27 +0100

    Update to code

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -40,6 +40,24 @@ def hbnb():
                            amenities=amenities,
                            places=places, cache_id=uuid.uuid4())
 
+@app.route('/')
+def hbnb_filters1(the_id=None):
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('2-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users)
+
 
 if __name__ == "__main__":
     """ Main Function """
